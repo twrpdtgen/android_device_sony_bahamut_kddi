e9bddc54c973e4b0423fd20047189fdaaff82ef2
plente00 <123705363+plente00@users.noreply.github.com>
2023-03-06 06:36:16 +0700

    Update and rename omni_bahamut_kddi.mk to twrp_bahamut_kddi.mk

--- twrp_bahamut_kddi.mk
+++ twrp_bahamut_kddi.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from bahamut_kddi device
 $(call inherit-product, device/sony/bahamut_kddi/device.mk)
 
 PRODUCT_DEVICE := bahamut_kddi
-PRODUCT_NAME := omni_bahamut_kddi
+PRODUCT_NAME := twrp_bahamut_kddi
 PRODUCT_BRAND := KDDI
 PRODUCT_MODEL := Bahamut_kddi
 PRODUCT_MANUFACTURER := sony
