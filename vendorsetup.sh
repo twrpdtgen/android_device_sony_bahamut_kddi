f7a8e6bd5ddde2129aa577fb749fa416e3afdd14
plente00 <123705363+plente00@users.noreply.github.com>
2023-03-06 06:37:36 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_bahamut_kddi-user
-add_lunch_combo omni_bahamut_kddi-userdebug
-add_lunch_combo omni_bahamut_kddi-eng
+add_lunch_combo twrp_bahamut_kddi-user
+add_lunch_combo twrp_bahamut_kddi-userdebug
+add_lunch_combo twrp_bahamut_kddi-eng
